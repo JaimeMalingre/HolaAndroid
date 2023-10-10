10597ee0c0be8346a1149fa2d5f1d9d9cd716e98
Jaime Malingre <jaimemalingre@gmail.com>
2023-10-10 14:57:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/holamundo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.holamundo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private int resultado = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMostrarMensaje = findViewById(R.id.buttonHello);
+        final TextView tvHolaMundo = findViewById(R.id.helloWorld);
+        final Button btnSumar = findViewById(R.id.botonSumar);
+        final TextView tvResultado = findViewById(R.id.txt_resultado);
+        Button botonReinicio = findViewById(R.id.botonReinicio);
+
+        btnMostrarMensaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Actualizar el texto del TextView cuando se hace clic en el botón
+                tvHolaMundo.setText("¡Hola Mundo!");
+
+            }
+        });
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sumar 1 + 1
+                resultado = resultado + 1;
+                // Mostrar el resultado en el TextView
+                tvResultado.setText("Resultado: " + resultado);
+            }
+        });
+        botonReinicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Reiniciar el contador a 0
+                resultado = 0;
+                // Mostrar el resultado reiniciado en el TextView
+                tvResultado.setText("Resultado: " + resultado);
+            }
+        });
+    }
+}
+
+
+
+
